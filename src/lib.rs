1b396edea2a57e110c065e49e01a750b0bf61e3f
Paolo Tagliaferri <paolo1.tagliaferri@gmail.com>
2025-06-27 22:28:49 +0200

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,18 @@
+#[macro_use]
+extern crate log;
+
+pub fn add(left: u64, right: u64) -> u64 {
+    log::info!("Adding {} and {}", left, right);
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
