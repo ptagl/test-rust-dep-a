9323fdbd546b8e2d8a4ac2fac0880953e4898c4b
Paolo Tagliaferri <paolo1.tagliaferri@gmail.com>
2025-06-27 22:53:56 +0200

    Minor source doc update

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 #[macro_use]
 extern crate log;
 
+/// A simple function that adds two numbers together.
 pub fn add(left: u64, right: u64) -> u64 {
     log::info!("Adding {} and {}", left, right);
     left + right
